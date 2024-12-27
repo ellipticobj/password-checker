47a84f39616ceb25d8d34d6f4faa69f3ef94617e
luna :3 <ellipticobj@gmail.com>
2024-12-27 16:48:12 +0800

    error cleanup thing??

--- secretpassword.py
+++ secretpassword.py
@@ -15,27 +15,28 @@ def getpassword(prompt="Input password: ",showasterisk=False):
     # save settings of terminal before i set it to raw input
     oldsettings = termios.tcgetattr(stdinfd)
     
-    tty.setraw(stdinfd)
-    
-    while True:
-        char = sys.stdin.read(1)
-        # break if user presses enter
-        if char == "\n" or char == "\r":
-            break
-        # remove one character from the password if backspace is pressed
-        elif char == "\x7f": 
-            if password:
-                password = password[:-1]
+    try:
+        tty.setraw(stdinfd)
+        
+        while True:
+            char = sys.stdin.read(1)
+            # break if user presses enter
+            if char == "\n" or char == "\r":
+                break
+            # remove one character from the password if backspace is pressed
+            elif char == "\x7f": 
+                if password:
+                    password = password[:-1]
+                    if showasterisk:
+                        # removes last character
+                        sys.stdout.write('\b \b')
+                        sys.stdout.flush()
+            else:
+                password += char
                 if showasterisk:
-                    # removes last character
-                    sys.stdout.write('\b \b')
+                    sys.stdout.write("*")
                     sys.stdout.flush()
-        else:
-            password += char
-            if showasterisk:
-                sys.stdout.write("*")
-                sys.stdout.flush()
-    
-    termios.tcsetattr(stdinfd, termios.TCSADRAIN, oldsettings)
+    finally:
+        termios.tcsetattr(stdinfd, termios.TCSADRAIN, oldsettings)
 
     return password
